606e7c7c00d3fc60d3bfa70214c82feea80081d1
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2021-06-21 18:01:16 +0200

    update: minor changes (main.s, makefile)

--- code/intro/riscv-elf-pic/main.s
+++ code/intro/riscv-elf-pic/main.s
@@ -4,6 +4,8 @@
 	.attribute unaligned_access, 0
 	.attribute stack_align, 16
 	.text
+.Ltext0:
+	.cfi_sections	.debug_frame
 	.section	.rodata.str1.8,"aMS",@progbits,1
 	.align	3
 .LC0:
@@ -13,15 +15,33 @@
 	.globl	main
 	.type	main, @function
 main:
+.LFB3:
+	.file 1 "main.c"
+	.loc 1 5 12
+	.cfi_startproc
+	.loc 1 6 3
+	.loc 1 5 12 is_stmt 0
 	addi	sp,sp,-16
+	.cfi_def_cfa_offset 16
+	.loc 1 6 3
 	la	a1,x
 	lla	a0,.LC0
+	.loc 1 5 12
 	sd	ra,8(sp)
+	.cfi_offset 1, -8
+	.loc 1 6 3
 	call	printf@plt
+.LVL0:
+	.loc 1 7 3 is_stmt 1
+	.loc 1 8 1 is_stmt 0
 	ld	ra,8(sp)
+	.cfi_restore 1
 	li	a0,0
 	addi	sp,sp,16
+	.cfi_def_cfa_offset 0
 	jr	ra
+	.cfi_endproc
+.LFE3:
 	.size	main, .-main
 	.globl	x
 	.data
@@ -30,4 +50,293 @@ main:
 	.size	x, 4
 x:
 	.word	1
+	.text
+.Letext0:
+	.file 2 "/opt/riscv/toolchain/riscv64-unknown-elf/include/stdio.h"
+	.section	.debug_info,"",@progbits
+.Ldebug_info0:
+	.4byte	0xe5
+	.2byte	0x4
+	.4byte	.Ldebug_abbrev0
+	.byte	0x8
+	.byte	0x1
+	.4byte	.LASF11
+	.byte	0xc
+	.4byte	.LASF12
+	.4byte	.LASF13
+	.4byte	.Ldebug_ranges0+0
+	.8byte	0
+	.4byte	.Ldebug_line0
+	.byte	0x2
+	.byte	0x1
+	.byte	0x6
+	.4byte	.LASF0
+	.byte	0x2
+	.byte	0x1
+	.byte	0x8
+	.4byte	.LASF1
+	.byte	0x2
+	.byte	0x2
+	.byte	0x5
+	.4byte	.LASF2
+	.byte	0x2
+	.byte	0x2
+	.byte	0x7
+	.4byte	.LASF3
+	.byte	0x3
+	.byte	0x4
+	.byte	0x5
+	.string	"int"
+	.byte	0x2
+	.byte	0x4
+	.byte	0x7
+	.4byte	.LASF4
+	.byte	0x2
+	.byte	0x8
+	.byte	0x5
+	.4byte	.LASF5
+	.byte	0x2
+	.byte	0x8
+	.byte	0x7
+	.4byte	.LASF6
+	.byte	0x2
+	.byte	0x8
+	.byte	0x5
+	.4byte	.LASF7
+	.byte	0x2
+	.byte	0x10
+	.byte	0x4
+	.4byte	.LASF8
+	.byte	0x2
+	.byte	0x1
+	.byte	0x8
+	.4byte	.LASF9
+	.byte	0x2
+	.byte	0x8
+	.byte	0x7
+	.4byte	.LASF10
+	.byte	0x4
+	.string	"x"
+	.byte	0x1
+	.byte	0x3
+	.byte	0x5
+	.4byte	0x45
+	.byte	0x9
+	.byte	0x3
+	.8byte	x
+	.byte	0x5
+	.4byte	.LASF14
+	.byte	0x1
+	.byte	0x5
+	.byte	0x5
+	.4byte	0x45
+	.8byte	.LFB3
+	.8byte	.LFE3-.LFB3
+	.byte	0x1
+	.byte	0x9c
+	.4byte	0xdc
+	.byte	0x6
+	.8byte	.LVL0
+	.4byte	0xdc
+	.byte	0x7
+	.byte	0x1
+	.byte	0x5a
+	.byte	0x9
+	.byte	0x3
+	.8byte	.LC0
+	.byte	0x7
+	.byte	0x1
+	.byte	0x5b
+	.byte	0x9
+	.byte	0x3
+	.8byte	x
+	.byte	0
+	.byte	0
+	.byte	0x8
+	.4byte	.LASF15
+	.4byte	.LASF15
+	.byte	0x2
+	.byte	0xc8
+	.byte	0x5
+	.byte	0
+	.section	.debug_abbrev,"",@progbits
+.Ldebug_abbrev0:
+	.byte	0x1
+	.byte	0x11
+	.byte	0x1
+	.byte	0x25
+	.byte	0xe
+	.byte	0x13
+	.byte	0xb
+	.byte	0x3
+	.byte	0xe
+	.byte	0x1b
+	.byte	0xe
+	.byte	0x55
+	.byte	0x17
+	.byte	0x11
+	.byte	0x1
+	.byte	0x10
+	.byte	0x17
+	.byte	0
+	.byte	0
+	.byte	0x2
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0xe
+	.byte	0
+	.byte	0
+	.byte	0x3
+	.byte	0x24
+	.byte	0
+	.byte	0xb
+	.byte	0xb
+	.byte	0x3e
+	.byte	0xb
+	.byte	0x3
+	.byte	0x8
+	.byte	0
+	.byte	0
+	.byte	0x4
+	.byte	0x34
+	.byte	0
+	.byte	0x3
+	.byte	0x8
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x2
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x5
+	.byte	0x2e
+	.byte	0x1
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0x49
+	.byte	0x13
+	.byte	0x11
+	.byte	0x1
+	.byte	0x12
+	.byte	0x7
+	.byte	0x40
+	.byte	0x18
+	.byte	0x97,0x42
+	.byte	0x19
+	.byte	0x1
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x6
+	.byte	0x89,0x82,0x1
+	.byte	0x1
+	.byte	0x11
+	.byte	0x1
+	.byte	0x31
+	.byte	0x13
+	.byte	0
+	.byte	0
+	.byte	0x7
+	.byte	0x8a,0x82,0x1
+	.byte	0
+	.byte	0x2
+	.byte	0x18
+	.byte	0x91,0x42
+	.byte	0x18
+	.byte	0
+	.byte	0
+	.byte	0x8
+	.byte	0x2e
+	.byte	0
+	.byte	0x3f
+	.byte	0x19
+	.byte	0x3c
+	.byte	0x19
+	.byte	0x6e
+	.byte	0xe
+	.byte	0x3
+	.byte	0xe
+	.byte	0x3a
+	.byte	0xb
+	.byte	0x3b
+	.byte	0xb
+	.byte	0x39
+	.byte	0xb
+	.byte	0
+	.byte	0
+	.byte	0
+	.section	.debug_aranges,"",@progbits
+	.4byte	0x2c
+	.2byte	0x2
+	.4byte	.Ldebug_info0
+	.byte	0x8
+	.byte	0
+	.2byte	0
+	.2byte	0
+	.8byte	.LFB3
+	.8byte	.LFE3-.LFB3
+	.8byte	0
+	.8byte	0
+	.section	.debug_ranges,"",@progbits
+.Ldebug_ranges0:
+	.8byte	.LFB3
+	.8byte	.LFE3
+	.8byte	0
+	.8byte	0
+	.section	.debug_line,"",@progbits
+.Ldebug_line0:
+	.section	.debug_str,"MS",@progbits,1
+.LASF7:
+	.string	"long long int"
+.LASF4:
+	.string	"unsigned int"
+.LASF14:
+	.string	"main"
+.LASF6:
+	.string	"long unsigned int"
+.LASF10:
+	.string	"long long unsigned int"
+.LASF11:
+	.string	"GNU C17 10.2.0 -mtune=rocket -march=rv64imafdc -mabi=lp64d -march=rv64imafdc -g -O3 -fpic"
+.LASF1:
+	.string	"unsigned char"
+.LASF9:
+	.string	"char"
+.LASF12:
+	.string	"main.c"
+.LASF13:
+	.string	"/root/local"
+.LASF5:
+	.string	"long int"
+.LASF3:
+	.string	"short unsigned int"
+.LASF15:
+	.string	"printf"
+.LASF8:
+	.string	"long double"
+.LASF2:
+	.string	"short int"
+.LASF0:
+	.string	"signed char"
 	.ident	"GCC: (GNU) 10.2.0"
