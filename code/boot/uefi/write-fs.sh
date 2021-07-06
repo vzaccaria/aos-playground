253c192f20d42da240520f52ee0e180a0a288350
Vittorio Zaccaria <vittorio.zaccaria@gmail.com>
2021-07-06 15:10:56 +0200

    update: minor changes (write-fs.sh)

--- code/boot/uefi/write-fs.sh
+++ code/boot/uefi/write-fs.sh
@@ -2,7 +2,9 @@ mkfs.vfat -F 32 /dev/loop0p2
 mkfs.ext4 /dev/loop0p3
 mkdir -p /efi
 mount /dev/loop0p2 /efi
-cp uefi-kernel/bzImage.efi /efi
-cp uefi-kernel/initrd.img /efi
+mkdir -p /efi/boot
+cp uefi-kernel/bzImage.efi /efi/boot/bootx64.efi
+cp uefi-kernel/initrd.img /efi/boot
 umount /efi
+losetup -D
 
